a81bddc0c6cc4fd3eed7ee353bb424a6e4a93e6b
seokkuuu <seokkub@gmail.com>
2023-03-24 00:59:08 -0400

    Initial commit

---
+++ crud/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return str(self.title)
---
+++ crud/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+
+# Create your views here.
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
